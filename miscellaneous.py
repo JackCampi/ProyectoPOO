39225342054ebc3811a6f0dc56563d52ec06c754
JackCampi <bromeroa@unal.edu.co>
2019-09-27 11:54:45 -0500

    miscellaneous 1.1.5

--- miscellaneous.py
+++ miscellaneous.py
@@ -40,19 +40,65 @@ def SearchMainList(_format, name):
 def BinarySearch(_list, item):
     elementsFound = []
     for key in _list[0].keys():
+        soartedList = SortList(_list, key)
         first = 0
-        last = len(_list)-1
-        while first <= last:
+        last = len(soartedList)-1
+        found = False
+        while first <= last and not found:
             middle = (first + last) // 2
-            if _list[middle][key] == item:
-                elementsFound.append(_list[middle])
-                first = middle + 1
+            if soartedList[middle][key] == item:
+                sameItemRight = CheckRight(soartedList, middle, item, key)
+                sameItemLeft = CheckLeft(soartedList, middle, item, key)-1
+                elementsFound = soartedList[middle-sameItemLeft:middle+sameItemRight]
+                #Clean(elementsFound)
+                found = True
             else:
-                if item < _list[middle][key]:
+                if item < soartedList[middle][key]:
                     last = middle - 1
                 else:
                     first = middle + 1
     return elementsFound
 
+def CheckRight(_list, index, item, key):
+    if _list[index][key] != item:
+        return 0
+    else:
+        #answer.append(_list[index])
+        #answer.append(CheckSides(_list, index + 1, item, key, answer))
+
+        #return list(_list[index]) + list(CheckSides(_list, index + 1, item, key)) + list(CheckSides(_list, index - 1, item, key))
+        #return list(_list[index]).append(CheckSides(_list, index + 1, item, key)) #+ list(CheckSides(_list, index - 1, item, key))
+        #print(key, item, index)
+        #print(_list)
+        if index == len(_list)-1:
+            return 1
+        else:
+            return 1 + CheckRight(_list, index + 1, item, key) #+ CheckSides(_list, index - 1, item, key)
+
+def CheckLeft(_list, index, item, key):
+    if _list[index][key] != item:
+        return 0
+    else:
+        #answer.append(_list[index])
+        #answer.append(CheckSides(_list, index + 1, item, key, answer))
+
+        #return list(_list[index]) + list(CheckSides(_list, index + 1, item, key)) + list(CheckSides(_list, index - 1, item, key))
+        #return list(_list[index]).append(CheckSides(_list, index + 1, item, key)) #+ list(CheckSides(_list, index - 1, item, key))
+        #print(key, item, index)
+        #print(_list)
+        if index == 0:
+            return 1
+        else:
+            return 1 + CheckLeft(_list, index - 1, item, key) #+ CheckSides(_list, index - 1, item, key)
+
+def Clean(_list):
+    for i in range(len(_list)-1):
+        if type(_list[i]) != type({}) or _list[i] == {}:
+            #.remove(_list[i])
+            _list.pop(i)
 #print(SortMainList("music","type"))
-print(SearchMainList("music", "thriller"))
+'''for i in SortMainList("music", "type"):
+    print(i)'''
+#print(SearchMainList("music", "pop"))
+for i in SearchMainList("music", "no 6 collaborations project"):
+    print(i)
