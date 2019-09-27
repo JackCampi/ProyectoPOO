8a31b6b7458fd80d348c14131c0cdb3373069d7b
JackCampi <bromeroa@unal.edu.co>
2019-09-27 08:54:42 -0500

    miscellaneous 1.1.1

--- miscellaneous.py
+++ miscellaneous.py
@@ -3,7 +3,7 @@
 import prueba
 
 def SortList(_format, key):
-    print("hola")
+
     mainList = prueba.GetList()
     #mainList = readFortmat(_format)
     try:
@@ -11,4 +11,28 @@ def SortList(_format, key):
     except:
         raise NameError("Invalid key, please check if the key is correct")
 
+def Search(_format, name):
+
+    mainList = SortList("music","name")
+    #mainList = readFortmat(_format)
+    item = BinarySearch(mainList, name)
+    return item
+
+def BinarySearch(_list, name):
+    first = 0
+    last = len(_list)-1
+    found = False
+    #return 34
+    while first <= last and not found:
+        middle = (first + last) // 2
+        if _list[middle]["name"] == name:
+            found = True
+        else:
+            if name < _list[middle]["name"]:
+                last = middle - 1
+            else:
+                first = middle + 1
+    return found
+
 print(SortList("music","name"))
+print(Search("music", "demons3"))
